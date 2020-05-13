7b8648ada1f4ae9f570074334beb1dce36369ec8
Srayan Gangopadhyay <sgango@users.noreply.github.com>
2020-05-13 20:15:23 +0100

    added integration and plotting

--- testing/euler.py
+++ testing/euler.py
@@ -1,21 +1,32 @@
 """
+Test implementation of Euler's method
 Srayan Gangopadhyay
 2020-05-13
-
-Test implementation of Euler's method
 """
 
 import scipy as sp
+import matplotlib.pyplot as plt
 
-def func(x):  # RHS of ODE in form dy/dx= 
-    return sp.exp(x)
+def func(y):  # RHS of ODE in form dy/dx= 
+    return y
 
 # PARAMETERS
 y0 = 1  # initial value
 delta = 1  # step size
-end = 10  # x-value to stop integration
+end = 3  # x-value to stop integration
 
-steps = int(end/delta)
-time = sp.linspace(0, end, steps+1)
+steps = int(end/delta)  # number of steps
+x = sp.linspace(0, end, steps+1)  # array of x-values
+y = sp.zeros(steps+1)  # empty array for solution
+y[0] = y0  # inserting initial value
 
-# TODO: write integrator
+# INTEGRATING
+for i in range(1, steps+1):
+    y[i] = y[i-1] + (delta*func(y[i-1]))
+
+plt.plot(x, y, label='Approx. soln (Euler)')
+plt.plot(x, y, 'o')
+# TODO: plot true solution for test cases
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
