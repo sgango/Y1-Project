65580212fdfd20a63b255b5ca36bd90b76f0e4a1
Srayan Gangopadhyay <sgango@users.noreply.github.com>
2020-05-17 15:35:03 +0100

    finished implementing RK4 algorithm

--- testing/runge-kutta/rk4.py
+++ testing/runge-kutta/rk4.py
@@ -2,7 +2,7 @@
 Upgrading Euler method to 4th-order Runge-Kutta
 for 2nd-order ODEs
 Srayan Gangopadhyay
-2020-05-16
+2020-05-17
 """
 
 import numpy as np
@@ -32,19 +32,21 @@ y[0] = y0  # inserting initial value
 v[0] = v0
 
 # INTEGRATING
-# https://mathworld.wolfram.com/Runge-KuttaMethod.html
+# using https://mathworld.wolfram.com/Runge-KuttaMethod.html
 for i in range(0, steps-1):
+    k1y = h * v[i]
     k1v = h * func(x[i], y[i], v[i])
+    k2y = h * (v[i] + 0.5*k1v)
     k2v = h * func((x[i] + 0.5*h), (y[i] + 0.5*k1v), (v[i] + 0.5*k1v))
+    k3y = h * (v[i] + 0.5*k2v)
     k3v = h * func((x[i] + 0.5*h), (y[i] + 0.5*k2v), (v[i] + 0.5*k2v))
+    k4y = h * (v[i] + k3v)
     k4v = h * func(x[i+1], (y[i] + k3v), (v[i] + k3v))
-
-    # TODO: calculate ks for y and add below
     
-    v[i+1] = v[i] + (h*func(x[i], y[i], v[i]))
-    y[i+1] = y[i] + (h*v[i])
+    y[i+1] = y[i] + (k1y + 2*k2y + 2*k3y + k4y) / 6
+    v[i+1] = v[i] + (k1v + 2*k2v + 2*k3v + k4v) / 6
 
-plt.plot(x, y, label='Approx. soln (Euler)')
+plt.plot(x, y, label='Approx. soln (RK4)')
 plt.plot(x, y, 'o')
 plt.xlabel('x')
 plt.ylabel('y')
